cd5717afff7fcca9497014dcc5a0562e074ebc6f
Ederson Caldato <c4ederson@gmail.com>
2025-10-23 20:45:24 -0300

    atualizado

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/layout/Header"
+import { Footer } from "./components/layout/Footer"
 
 function App() {
 
   return (
     <>
-      <Header />
+      <Header/>
+      <Footer/>
     </>
   )
 }
---
+++ the-moviebox-react/src/components/layout/Footer/index.tsx
@@ -0,0 +1,21 @@
+import { Facebook, Instagram, Youtube} from "lucide-react";
+
+export function Footer() {
+return (
+      <footer>
+    <div className="container">
+      <p>The MovieBox 2025 - Todos os direitos são reservados</p>
+      <ul>
+        <li>
+          <a href="#">
+          <Facebook/>  
+          </a></li>
+        <li><a href="#">
+          <Instagram/></a></li>
+        <li><a href="#">
+          <Youtube/></a></li>
+      </ul>
+    </div>    
+  </footer>
+)
+}
