0b2c6cb2a588fd531d66daa46a0127ce41b638ed
Ederson Caldato <c4ederson@gmail.com>
2025-11-06 20:12:48 -0300

    Update App.tsx

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/layout/header"
 import { Footer } from "./components/layout/footer"
 import { Navbar } from "./components/layout/navbar"
+import { MovieContent } from "./cases:movies/components/movie.content"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header/>
       <Navbar />
+      <MovieContent />
       <Footer/>
     </>
   )
